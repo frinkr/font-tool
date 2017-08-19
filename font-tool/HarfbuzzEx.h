94e2f4795a08e1b84373d16e8255aa567f61b604
Yuqing Jiang <frinkr@outlook.com>
2017-08-19 17:42:51 +0800

    Don't know why but harfbuzz-ex crashes, so make the same compiler flags with harfbuzz sources

--- font-tool/HarfbuzzEx.h
+++ font-tool/HarfbuzzEx.h
@@ -1,42 +1,40 @@
 #ifndef HARFBUZZ_EX_H
 #define HARFBUZZ_EX_H
-#include <hb-ot.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <hb-ot.h>
+//#include <hb-ot-layout.h>
+
+HB_BEGIN_DECLS
+
+/** return -1 if lookup not found **/
+HB_EXTERN unsigned int
+hb_ot_layout_lookup_get_type (hb_face_t    *face,
+                              hb_tag_t      table_tag,
+                              unsigned int  lookup_index);
     
-    /** return -1 if lookup not found **/
-    HB_EXTERN unsigned int
-    hb_ot_layout_lookup_get_type (hb_face_t    *face,
-                                  hb_tag_t      table_tag,
-                                  unsigned int  lookup_index);
+HB_EXTERN uint16_t
+hb_ot_layout_lookup_get_flag (hb_face_t    *face,
+                              hb_tag_t      table_tag,
+                              unsigned int  lookup_index);
     
-    HB_EXTERN uint16_t
-    hb_ot_layout_lookup_get_flag (hb_face_t    *face,
-                                  hb_tag_t      table_tag,
-                                  unsigned int  lookup_index);
-    
-    HB_EXTERN uint16_t
-    hb_ot_layout_lookup_get_mark_filtering_set (hb_face_t    *face,
-                                                hb_tag_t      table_tag,
-                                                unsigned int  lookup_index);
-    
-    HB_EXTERN unsigned int
-    hb_ot_layout_lookup_get_subtable_count (hb_face_t    *face,
+HB_EXTERN uint16_t
+hb_ot_layout_lookup_get_mark_filtering_set (hb_face_t    *face,
                                             hb_tag_t      table_tag,
                                             unsigned int  lookup_index);
     
+HB_EXTERN unsigned int
+hb_ot_layout_lookup_get_subtable_count (hb_face_t    *face,
+                                        hb_tag_t      table_tag,
+                                        unsigned int  lookup_index);
     
     
-    HB_EXTERN hb_set_t *
-    hb_ot_layout_lookup_get_subtable_coverage (hb_face_t    *face,
-                                               hb_tag_t      table_tag,
-                                               unsigned int  lookup_index,
-                                               unsigned int  subtable_index);
     
-#ifdef __cplusplus
-}
-#endif
+HB_EXTERN hb_set_t *
+hb_ot_layout_lookup_get_subtable_coverage (hb_face_t    *face,
+                                           hb_tag_t      table_tag,
+                                           unsigned int  lookup_index,
+                                           unsigned int  subtable_index);
 
+    
+HB_END_DECLS
 #endif
